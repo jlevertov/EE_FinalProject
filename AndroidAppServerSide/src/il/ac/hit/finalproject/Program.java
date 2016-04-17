5de1955e723293207c616a3ab563c301240302f5
jlevertov <jlevertov@yahoo.com>
2016-04-17 16:51:44 +0200

    asd

--- AndroidAppServerSide/src/il/ac/hit/finalproject/Program.java
+++ AndroidAppServerSide/src/il/ac/hit/finalproject/Program.java
@@ -6,11 +6,12 @@ public class Program {
 	
 	public static void main(String[] args) {
 		ToDoListItem item1 = new ToDoListItem("Test", "What house?", new Date(), new Date());
-		User item2 = new User(1, "Jalevertov", "BLAAAAAAAA", "jlevertov@walla.com", true);
+		User item2 = new User(1, "Japalevertov", "BLAAAAAAAA", "jlevertov@walla.com", true);
 		IDAO dao = new DAO();
 		try {
 			dao.AddListItem(item1);
-			//dao.AddUser(item2);
+			dao.AddUser(item2);
+			//dao.PrintUsers();
 		}
 		catch (ToDoListPlatformException e) {
 			// TODO Auto-generated catch block
